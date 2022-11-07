3fe4ddef6a58a47d16a3dd6197193608195f9ec0
unknown <villenaalfred@gmail.com>
2022-11-07 17:14:07 +0800

    fourth commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Kim Bianca love Cookies
+        Meredith the Ghost 
       </header>
     </div>
   );
