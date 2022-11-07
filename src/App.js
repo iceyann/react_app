4ee69c352123a693c96f9f9b711f889abf9f194b
unknown <villenaalfred@gmail.com>
2022-11-07 17:16:29 +0800

    5th commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Meredith the Ghost 
+        Biankim loves Meredith the Ghost
       </header>
     </div>
   );
